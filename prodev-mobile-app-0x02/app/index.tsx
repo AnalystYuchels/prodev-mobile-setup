85004bae4810d9d5e706aa642b7242a56b014e01
AnalystYuchels <uche4xploit@gmail.com>
2025-08-29 22:12:36 +0100

    Implementing safe areas, images, etc

---
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function Index(): JSX.Element {
+  const windowHeight = Dimensions.get("window").height;
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground
+          source={require("../assets/images/background-image.png")}
+          style={[styles.background, { height: windowHeight }]}
+          resizeMode="cover"
+        >
+          <View style={styles.container}>
+            <View style={styles.companyLogo}>
+              <Image source={require("../assets/images/Logo.png")} />
+            </View>
+
+            <View style={styles.textGroup}>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2</Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
+            </View>
+
+            <View style={styles.bottom}>
+              <View style={styles.buttonGroup}>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={{ ...styles.textSmall, color: "black" }}>
+                    Join here
+                  </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.transparentButton}>
+                  <Text style={styles.textSmall}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+
+              <View style={styles.continueWrap}>
+                <Text style={styles.continueText}>Continue to home</Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+  },
+  companyLogo: {
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+    marginBottom: 50,
+  },
+  textGroup: {
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  textLarge: {
+    color: "white",
+    fontWeight: "800",
+    fontSize: 40,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  textSmall: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "200",
+    textAlign: "center",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+  },
+  button: {
+    flex: 1,
+    marginRight: 10,
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  transparentButton: {
+    flex: 1,
+    marginLeft: 10,
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
+  bottom: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  continueWrap: {
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  continueText: {
+    color: "white",
+  },
+});
